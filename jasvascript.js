6584f3c551e2ede8dcf72e4972e6615828914e7c
julioslink <oscarleguizamo10.1@gmail.com>
2024-02-17 11:47:28 -0500

    subiendo

---
+++ jasvascript.js
@@ -0,0 +1,13 @@
+
+var primer_letra = document.getElementById("");
+var ancho = document.getElementById("ancho");
+var descuento = document.getElementById("descuento");
+var precio = document.getElementById("precio");
+var resultado = document.getElementById("resultado");
+
+function(suma=) {
+    suma =parseInt(num1.value);
+    suma=parseInt(num2.value);
+     
+    resultado =(num1+num2);
+    }
